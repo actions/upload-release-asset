63a40d604272f580f14b9ff2088658fd4ee10731
Thomas Hughes <iamhughes@github.com>
2019-09-25 11:57:54 -0500

    Create test stub, add CI workflow file, add test badge

---
+++ tests/main.test.js
@@ -0,0 +1,37 @@
+jest.mock('@actions/github');
+
+const { GitHub, context } = require('@actions/github');
+const run = require('../src/main.js');
+
+/* eslint-disable no-undef */
+describe('module', () => {
+    let uploadReleaseAsset;
+
+    beforeEach(() => {
+        uploadReleaseAsset = jest.fn();
+
+        context.repo = {
+            owner: 'owner',
+            repo: 'repo'
+        };
+
+        const github = {
+            repos: {
+                uploadReleaseAsset
+            }
+        };
+
+        GitHub.mockImplementation(() => github);
+    });
+
+    test('Upload release endpoint is called', async () => {
+        await run();
+
+        expect(uploadReleaseAsset).toHaveBeenCalledWith({
+            owner: 'owner',
+            repo: 'repo'
+        });
+    });
+
+    test('Outputs are set', async () => {});
+});
